fb8dceb8c43620f4a4d94317c3a39aa8c68ce727
Parker Catalano <catalano.work@gmail.com>
2017-11-10 18:41:39 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -29,8 +29,8 @@ class CashRegister
   end
 
   def void_last_transaction
-    self.total = total + (price * quantity)
-    self.total = total - @transaction[:price]
+    self.total = total - (@transaction[:price] * @transaction[:quantity])
+    
   end
 
 end
