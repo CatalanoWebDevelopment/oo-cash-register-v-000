463f207a9b2a69f8cbeec8a2b42de90c46b1ee71
Parker Catalano <catalano.work@gmail.com>
2017-11-10 17:21:39 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+class CashRegister
+  attr_accessor :cash_register_with_discount, :discount, :total
 
+  def initialize(cash_register_with_discount=0)
+    @discount = cash_register_with_discount
+    @total = 0
+  end
+
+  def add_item(title, price, quantity=1)
+    self.total = total + (price * quantity)
+  end
+
+  def apply_discount
+    total_discount = total * (discount/100).to_f
+    self.total = total - total_discount
+  end
+
+end
